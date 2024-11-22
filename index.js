8defc1c9df265f9a05234b46a11abe6d578884b2
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-11-23 00:47:01 +0530

    Fixed input box value

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const apikey = '1caf30a44acf505e3a3f152976906a4c';
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?units=metric&';
 
-const city = document.querySelector('.text').value;
+const city = document.querySelector('.city_name').value;
 const btn = document.querySelector('.search_button');
+console.log(city);
+
 //const cityName = city.value;
 
 async function weatherReport() {
