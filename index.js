72e76345e4d37eed51ef6102210d8927dd23e353
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-11-23 14:20:12 +0530

    Add: Error Message for wrong city name and Added input box clearing on click of search.

--- index.js
+++ index.js
@@ -6,13 +6,24 @@ const btn = document.querySelector('.search_button');
 
 async function weatherReport(cityName) {
     const data = await fetch(apiURL + `apikey=${apikey}` + '&q=' + cityName);
-    const jsonData = await data.json();
 
-    console.log(jsonData);
+    if (data.status == 404){
+        document.querySelector('.error').style.display = 'block'
+    }
     
+    const jsonData = await data.json();
+    document.querySelector('.Weather').style.display = 'block'
+    //console.log(jsonData);
+
+    document.querySelector('.temp').innerHTML = Math.round(jsonData.main.temp) + '°C';
+    document.querySelector('.city').innerHTML = jsonData.name
+    document.querySelector('.humidity').innerHTML = jsonData.main.humidity + '%'
+    document.querySelector('.wind').innerHTML = jsonData.wind.speed + 'km/h'
 }
 
 btn.addEventListener('click', ()=>{
     weatherReport(city.value);
     //console.log(city.value);
+    city.value = ''
 })
+
